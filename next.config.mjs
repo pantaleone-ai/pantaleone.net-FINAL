8521aeb9c7cd07bf284d4463b4109377afcd4f39
pantaleone-ai <mdptrading@gmail.com>
2025-02-22 11:30:15 -0700

    testing next/mdx (doesnt do anything)

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
+import createMDX from '@next/mdx'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+   // Configure `pageExtensions` to include markdown and MDX files
+   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+
   // Force dark mode
-  darkMode: 'class', 
+  darkMode: 'class',
   defaultTheme: 'dark',
     images: {
     domains: ['pantaleone-net.s3.us-west-1.amazonaws.com']
   }
 };
 
-export default nextConfig;
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
