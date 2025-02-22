470e475e789d1e637d552ad75eca6c109c00cdfa
pantaleone.net <96348408+pantaleone-ai@users.noreply.github.com>
2025-02-21 18:50:54 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   // Force dark mode
   darkMode: 'class', 
-  defaultTheme: 'dark' 
+  defaultTheme: 'dark'
+    images: {
+    domains: ['pantaleone-net.s3.us-west-1.amazonaws.com']
+  }
 };
 
 export default nextConfig;
