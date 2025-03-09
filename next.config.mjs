cc560b0bd7c7155b9115cc631e3688ee8877850b
pantaleone-ai <mdptrading@gmail.com>
2025-03-09 16:00:36 -0600

    redirect old /p, /tag to /

--- next.config.mjs
+++ next.config.mjs
@@ -3,31 +3,48 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-   // Configure `pageExtensions` to include markdown and MDX files
-   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
-
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  
   // Force dark mode
   darkMode: 'class',
   defaultTheme: 'dark',
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'pantaleone-net.s3.us-west-1.amazonaws.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-      localPatterns: [
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'pantaleone-net.s3.us-west-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+    localPatterns: [
       {
         pathname: '/images/**',
       },
     ],
-  }
+  },
+  
+  // Add redirects here
+  async redirects() {
+    return [
+      {
+        source: '/p/:path*',           // The path you want to redirect from
+        destination: '/',      // The path you want to redirect to
+        permanent: true,              // true for 301 (permanent), false for 307 (temporary)
+      },
+      {
+        source: '/tag/:path*',        // Using path parameters
+        destination: '/',
+        permanent: true,
+      },
+      // Add more redirect rules as needed
+    ]
+  },
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, as desired
   extension: /\.mdx?$/,
   options: {
     remarkPlugins: [],
@@ -35,5 +52,4 @@ const withMDX = createMDX({
   },  
 })
  
-// Merge MDX config with Next.js config
 export default withMDX(nextConfig)
